eff69fe0d2992a664a6a42120e1ee3edfea49eee
youaokok <zzhl10010@gmail.com>
2025-09-20 15:41:01 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -32,6 +32,8 @@ if [ -d *"luci-theme-argon"* ]; then
 
 	sed -i "/font-weight:/ { /important/! { /\/\*/! s/:.*/: var(--font-weight);/ } }" $(find ./luci-theme-argon -type f -iname "*.css")
 	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
+    sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/" ./luci-app-argon-config/root/etc/config/argon
+
 
 	cd $PKG_PATH && echo "theme-argon has been fixed!"
 fi
